6432b5ab421c968f362a1ce23657ee35d120ef04
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-01-17 11:01:43 -0700

    50/50 all functions complete

--- lib.js
+++ lib.js
@@ -31,51 +31,52 @@ lib.generateUnorderedList = function(arrayOfText) {
 
 lib.generateTableRow = function (arrayOfText) {
     var outStr = '<tr>';
-    for (var i = 0 ; i < arrayOfText.length ; i++){
+    for (var i = 0; i < arrayOfText.length; i++) {
         outStr = outStr + '<td>' + arrayOfText[i] + '</td>';
     }
-
-    outStr = outStr + '</tr>'
+    outStr = outStr + '</tr>';
     return outStr;
 
 }
 
 lib.generateTable = function(twoDimensionalArrayOfText) {
     var outStr = '<table>';
-	for (var i = 0; i < twoDimensionalArrayOfText.length ; i++){
-	    outStr = outStr + '<tr>';
-	    for (var j = 0; j < twoDimensionalArrayOfText[0].length ; j++){
-		    outStr = outStr + '<td>' + twoDimensionalArrayOfText[i][j] + '</td>';
-		}
-		outStr = outStr + '</tr>';
-	}
-	outStr = outStr + '</table>';
+    var len = twoDimensionalArrayOfText.length;
+    for(var i = 0; i < len; i++){
+        outStr = outStr + '<tr>';
+        for (var j = 0; j < twoDimensionalArrayOfText[i].length; j++) {
+            outStr = outStr + '<td>' + twoDimensionalArrayOfText[i][j] + '</td>';
+        }
+        outStr = outStr + '</tr>';
+    }
+    outStr = outStr + '</table>';
     return outStr;
 }
 
 lib.generateHyperLink = function(url, text) {
-    return "<a href=\"" + url + ">" + text + "</a>"
+    return "<a href=\"" + url + "\">" + text + "</a>";
 }
 
 lib.generateFormTextField = function(name) {
-    return "<input type=\"text\" name=\"" + name + "\">"
+    return "<input type=\"text\" name=\"" + name + "\">";
 }
 
 lib.generateFormTextFieldWithLabel = function(name, label) {
-    return "<label>" + label + "</label>" + " " + "<input type=\"text\" name=\"" + name + "\">"
+    return "<label>" + label + "</label>" + " " + "<input type=\"text\" name=\"" + name + "\">";
 }
 
 lib.generateDropdownList = function(arrayOfValues, arrayOfText) {
-    var outStr = "<select>\n"
-    for (var i = 0; i < arrayOfValues.length; i++) {
-		outStr = outStr + "\t<option value=\"" + arrayOfValues[i] + "\">" + arrayOfText[i] + "</option>\n"
-	}
-    return outStr + "</select>"
+    var outStr = "<select>\n";
+    var len = (arrayOfValues.length <= arrayOfText.length) ? arrayOfValues.length : arrayOfText.length;
+    for (var i = 0; i < len; i++) {
+        outStr = outStr + "\t<option value=\"" + arrayOfValues[i] + "\">" + arrayOfText[i] + "</option>\n";
+    }
+    return outStr + "</select>";
 }
 
 lib.generateYoutubeVideoEmbeddableFrame = function(width, height, videoId, allowfullscreen) {
-	var full = allowfullscreen ? " allowfullscreen" : ""
-    return "<iframe width=\"" + width + "\" height=\"" + height + "\" src=//www.youtube.com/embed/" + videoId + full + "></iframe>"
+    var full = allowfullscreen ? " allowfullscreen" : "";
+    return "<iframe width=\"" + width + "\" height=\"" + height + "\" src=\"//www.youtube.com/embed/" + videoId + "\"" + full + "></iframe>";
 }
 
 module.exports = lib
